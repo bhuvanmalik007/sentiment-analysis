5ce95f5e86d21dba10a70e20f9b597e5bccb5eee
bhuvan malik <bhuvanmalik007@gmail.com>
2017-05-01 11:17:57 +0530

    added sample comments

---
+++ samplecomments.js
@@ -0,0 +1,7 @@
+{
+  comments: [
+    "If your hunt for the finest pizzas has taken you to all sorts of newbies and still you haven't found the perfect one, head to Tossin!",
+    "Absolutely in love with this place. Ordered the peri peri chicken pizza, chicken calzone and Nutella and hazelnut calzone for dessert.",
+    "Nice interiors, well behaved attendants, cool location and super awesome food."
+  ]
+}
